65bc4ab20a3d47f3ccf6e07f17baeafc57027ab6
Simon Marache-Francisco <simon@marache.net>
2013-07-11 13:34:21 +0200

    first working commit : can intercept a SMS, and will start a new view if it contains a specific code.

--- gen/com/example/smsbeacon/R.java
+++ gen/com/example/smsbeacon/R.java
@@ -24,17 +24,18 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int EditText01=0x7f080005;
         public static final int LinearLayout1=0x7f080000;
         public static final int TextView01=0x7f080002;
         public static final int action_settings=0x7f080007;
-        public static final int editText1=0x7f080003;
+        public static final int edit_phone_number=0x7f080005;
+        public static final int edit_sms_code=0x7f080003;
         public static final int textView1=0x7f080004;
         public static final int textView2=0x7f080001;
         public static final int toggleButton1=0x7f080006;
     }
     public static final class layout {
         public static final int activity_config=0x7f030000;
+        public static final int activity_ringer=0x7f030001;
     }
     public static final class menu {
         public static final int config=0x7f070000;
