153d870c79e65a2c533b73f7019b1fc73dba0258
clementdaviller <clementdaviller@gmail.com>
2013-07-12 13:15:01 +0200

    ajout settings de l'application

--- gen/com/example/smsbeacon/R.java
+++ gen/com/example/smsbeacon/R.java
@@ -8,6 +8,10 @@
 package com.example.smsbeacon;
 
 public final class R {
+    public static final class array {
+        public static final int listOptions=0x7f050000;
+        public static final int listValues=0x7f050001;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,36 +21,41 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f060000;
+        public static final int activity_vertical_margin=0x7f060001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int EditText01=0x7f080005;
-        public static final int LinearLayout1=0x7f080000;
-        public static final int TextView01=0x7f080002;
-        public static final int action_settings=0x7f080007;
-        public static final int editText1=0x7f080003;
-        public static final int textView1=0x7f080004;
-        public static final int textView2=0x7f080001;
-        public static final int toggleButton1=0x7f080006;
+        public static final int LinearLayout1=0x7f0a0000;
+        public static final int TextView01=0x7f0a0002;
+        public static final int action_settings=0x7f0a000a;
+        public static final int delete_me=0x7f0a0009;
+        public static final int demo=0x7f0a0007;
+        public static final int edit_phone_number=0x7f0a0005;
+        public static final int edit_sms_code=0x7f0a0003;
+        public static final int prefs_access_btn=0x7f0a0006;
+        public static final int textView1=0x7f0a0004;
+        public static final int textView2=0x7f0a0001;
+        public static final int toggleButton1=0x7f0a0008;
     }
     public static final class layout {
         public static final int activity_config=0x7f030000;
     }
     public static final class menu {
-        public static final int config=0x7f070000;
+        public static final int config=0x7f090000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int hello_world=0x7f050002;
-        public static final int lbl_sms_code=0x7f050004;
-        public static final int lbl_tel_number=0x7f050005;
-        public static final int lbl_title_trigger=0x7f050006;
-        public static final int toggle_on_off=0x7f050003;
+        public static final int action_settings=0x7f070001;
+        public static final int app_name=0x7f070000;
+        public static final int demo=0x7f070008;
+        public static final int hello_world=0x7f070002;
+        public static final int lbl_sms_code=0x7f070004;
+        public static final int lbl_tel_number=0x7f070005;
+        public static final int lbl_title_trigger=0x7f070006;
+        public static final int settings=0x7f070007;
+        public static final int toggle_on_off=0x7f070003;
     }
     public static final class style {
         /** 
@@ -70,10 +79,13 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f080000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f080001;
+    }
+    public static final class xml {
+        public static final int prefs=0x7f040000;
     }
 }
