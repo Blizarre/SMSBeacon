54b1cf7b3040dbfec1c3fef0920044621d533136
clementdaviller <clementdaviller@gmail.com>
2013-07-13 20:54:39 +0200

    reglage du son au maximum.

--- src/com/example/smsbeacon/RingActivity.java
+++ src/com/example/smsbeacon/RingActivity.java
@@ -2,6 +2,7 @@ package com.example.smsbeacon;
 
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
@@ -9,81 +10,80 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 import android.app.Activity;
+import android.content.Context;
 import android.content.pm.PackageManager;
 
 public class RingActivity extends Activity {
-	Camera cam = null;
-	boolean hasFlash = false;
-	Parameters p = null;
-	Ringtone r = null;
+	private Camera m_cam = null;
+	private boolean m_hasFlash = false;
+	private Parameters m_param = null;
+	private Ringtone m_rgtone = null;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ring);
-		
 		Button Bn;
-		Bn = (Button)findViewById(R.id.stop);
 		
+		Bn = (Button)findViewById(R.id.stop);		
 		Bn.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
 				turnOffFlash();
 				turnOffRingTone();
+				RingActivity.this.finish();
 			}
 		});
-		
-		hasFlash = getApplicationContext().getPackageManager()
+		m_hasFlash = getApplicationContext().getPackageManager()
 		        .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+		AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+		audioManager.setStreamVolume(
+				   AudioManager.STREAM_RING,
+				   audioManager.getStreamMaxVolume(AudioManager.STREAM_RING),
+				   AudioManager.FLAG_PLAY_SOUND
+				);
 		turnOnFlash();
 		turnOnRingTone();
-		
 	}
 	
 	protected void turnOnRingTone()
 	{
 		Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
-		r = RingtoneManager.getRingtone(getApplicationContext(), notification);
-		r.play();
+		m_rgtone = RingtoneManager.getRingtone(getApplicationContext(), notification);
+		m_rgtone.play();
 	}
 	
 	protected void turnOffRingTone()
 	{
-		r.stop();
+		m_rgtone.stop();
 	}
 	
 	protected void turnOnFlash()
 	{
-		if (hasFlash)
+		if (m_hasFlash)
 		{
 			try {
-					cam = Camera.open();     
-					p = cam.getParameters();
-					p.setFlashMode(Parameters.FLASH_MODE_TORCH);
-					cam.setParameters(p);
-					cam.startPreview();
+					m_cam = Camera.open();     
+					m_param = m_cam.getParameters();
+					m_param.setFlashMode(Parameters.FLASH_MODE_TORCH);
+					m_cam.setParameters(m_param);
+					m_cam.startPreview();
 				} catch (RuntimeException e) {
 	            Log.e("Camera Error. Failed to Open. Error: ", e.getMessage());
 	        }
-		}
-		else /*TODO: supprimer else inutile*/
-		{
-		Toast.makeText(RingActivity.this,
-				"Your device do not have flash light",
-				Toast.LENGTH_SHORT).show();
 		}
 	}
 	
 	protected void turnOffFlash()
 	{
-		if (cam == null || p == null) {
+		if (m_cam == null || m_param == null) {
             return;
         }
-        p.setFlashMode(Parameters.FLASH_MODE_OFF);
-        cam.setParameters(p);
-        cam.stopPreview();
+        m_param.setFlashMode(Parameters.FLASH_MODE_OFF);
+        m_cam.setParameters(m_param);
+        m_cam.stopPreview();
+        m_cam.release();
 	}
 
 }
