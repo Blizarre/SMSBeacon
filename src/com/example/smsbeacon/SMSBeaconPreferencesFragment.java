909883f1eacf18b6aee363077db501cf9bbbaf4e
Simon Marache-Francisco <simon@marache.net>
2013-07-17 22:26:00 +0200

    Now the app is back with no deprecation warning, and layout files back where they belongs

--- src/com/example/smsbeacon/SMSBeaconPreferencesFragment.java
+++ src/com/example/smsbeacon/SMSBeaconPreferencesFragment.java
@@ -1,15 +1,13 @@
 package com.example.smsbeacon;
 
-import android.preference.PreferenceActivity;
 import android.os.Bundle;
+import android.preference.PreferenceFragment;
 
 public class SMSBeaconPreferencesFragment extends PreferenceFragment {
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 	   super.onCreate(savedInstanceState);
-	   addPreferencesFromResource(R.xml.prefs);/*TODO: utiliser PreferenceFragment 
-	   											plutot que preference activity pour eviter 
-	   											l'obsolescence*/
+	   addPreferencesFromResource(R.layout.prefs);
 	}
 }
