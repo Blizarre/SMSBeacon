df095ca13bd078ba7ae066721724b773b8c6e1a9
Simon Marache-Francisco <simon@marache.net>
2013-07-17 09:08:51 +0200

    Use of string.xml . moreover, the preference activity code is used

--- src/com/example/smsbeacon/SMSReceiver.java
+++ src/com/example/smsbeacon/SMSReceiver.java
@@ -3,7 +3,9 @@ package com.example.smsbeacon;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.telephony.SmsMessage;
 import android.util.Log;
 
@@ -29,10 +31,10 @@ public class SMSReceiver extends BroadcastReceiver {
 				for ( Object o : smsextras) {
 					SmsMessage smsmsg = SmsMessage.createFromPdu((byte[])o);
 					String strMsgBody = smsmsg.getMessageBody();
-					if(strMsgBody.equals(getTriggerCodeSMS())) {
+					if(strMsgBody.equals(getTriggerCodeSMS(context))) {
 						lostCodeDetected(context);
 						break;
-					} else if(strMsgBody.equals(getTheftCodeSMS())) {
+					} else if(strMsgBody.equals(getTheftCodeSMS(context))) {
 						theftCodeDetected();
 						break;
 					}
@@ -46,12 +48,14 @@ public class SMSReceiver extends BroadcastReceiver {
 
 
 
-	private String getTriggerCodeSMS() {
-		return "aaa";
+	private String getTriggerCodeSMS(Context c) {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(c);
+		return preferences.getString(c.getString(R.string.pref_key_lost_passwd), "");
 	}
 
-	private String getTheftCodeSMS() {
-		return "bbb";
+	private String getTheftCodeSMS(Context c) {
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(c);
+		return preferences.getString(c.getString(R.string.pref_key_theft_passwd), "");
 	}
 	
 	private void  lostCodeDetected(Context context) {
