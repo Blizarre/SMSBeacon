411d141495c8651953018c0273880f934b02b3bb
tnemelc <clement.daviller@gmail.com>
2013-07-18 22:25:44 +0200

    affichage coordonée gps

--- src/com/example/smsbeacon/RingActivity.java
+++ src/com/example/smsbeacon/RingActivity.java
@@ -1,7 +1,13 @@
 package com.example.smsbeacon;
 
+import java.util.ArrayList;
+
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationManager;
+import android.location.LocationProvider;
 import android.media.AudioManager;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
@@ -10,6 +16,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 import android.app.Activity;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -53,6 +60,7 @@ public class RingActivity extends Activity {
 				);
 		turnOnFlash();
 		turnOnRingTone();
+		displayCoordinates();
 	}
 	
 	protected void turnOnRingTone()
@@ -94,4 +102,44 @@ public class RingActivity extends Activity {
         m_cam.release();
 	}
 
+	//fonction à déplacer dans le service
+	//une fois que t'auras fait ton boulot ;p
+	protected void displayCoordinates()
+	{
+		double lon, lat, alt;
+		LocationManager locationManager = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+		ArrayList<LocationProvider> providers = new ArrayList<LocationProvider>();
+		ArrayList<String> names = (ArrayList<String>) locationManager.getProviders(true);
+		 
+		for(String name : names)
+		  providers.add(locationManager.getProvider(name));
+		
+		Criteria critere = new Criteria();
+		critere.setAccuracy(Criteria.ACCURACY_FINE);
+		critere.setAltitudeRequired(false);
+		critere.setBearingRequired(false);
+		critere.setCostAllowed(false);
+		// Criteria.POWER_HIGH pour une haute consommation, 
+		// Criteria.POWER_MEDIUM pour une consommation moyenne 
+		// et Criteria.POWER_LOW pour une basse consommation
+		critere.setPowerRequirement(Criteria.POWER_HIGH);
+		critere.setSpeedRequired(true);
+		
+		String bestProvider = locationManager.getBestProvider(critere, false);
+		Location location = locationManager.getLastKnownLocation(bestProvider);
+	    try {
+	        lat = location.getLatitude();
+	        lon = location.getLongitude();
+	        alt = location.getAltitude();
+	    } catch (NullPointerException e) {
+	        lat = -1.0;
+	        lon = -1.0;
+	        alt = -1.0;
+	    }
+	    String positionStr = "longitude: " + lon + 
+	    					 "\n latitude: " + lat +
+	    					 "\n altitude:" + alt; 
+	    Toast.makeText(this, positionStr, Toast.LENGTH_LONG).show();
+	}
+	    
 }
--- src/com/example/smsbeacon/SMSBeaconPreferenceActivity.java
+++ src/com/example/smsbeacon/SMSBeaconPreferenceActivity.java
@@ -2,10 +2,15 @@ package com.example.smsbeacon;
 
 import android.app.Fragment;
 import android.app.FragmentTransaction;
+import android.database.Cursor;
+import android.media.RingtoneManager;
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
 
 public class SMSBeaconPreferenceActivity extends PreferenceActivity {
+	RingtoneManager mRingtoneManager;
+	Cursor mcursor;
+	String title;
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 
@@ -14,6 +19,11 @@ public class SMSBeaconPreferenceActivity extends PreferenceActivity {
 			Fragment newFragment = new SMSBeaconPreferencesFragment();
 			FragmentTransaction ft = getFragmentManager().beginTransaction();
 			ft.add(android.R.id.content, newFragment).commit();
-		}
+			mRingtoneManager = new RingtoneManager(this);
+			mcursor = mRingtoneManager.getCursor();
+			title = mRingtoneManager.EXTRA_RINGTONE_TITLE;
+	}
 	}
 }
+
+
