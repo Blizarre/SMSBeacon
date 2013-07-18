a998704f7b27dc8d38f767aff78630c5e0d672d8
tnemelc <clement.daviller@gmail.com>
2013-07-18 12:42:40 +0200

    MAJ strings.xml

--- src/com/example/smsbeacon/PreferencesActivity.java
+++ src/com/example/smsbeacon/PreferencesActivity.java
@@ -1,6 +1,8 @@
 package com.example.smsbeacon;
 
+import android.preference.ListPreference;
 import android.preference.PreferenceActivity;
+import android.media.RingtoneManager;
 import android.os.Bundle;
 
 public class PreferencesActivity extends PreferenceActivity {
@@ -8,8 +10,10 @@ public class PreferencesActivity extends PreferenceActivity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 	   super.onCreate(savedInstanceState);
-	   addPreferencesFromResource(R.xml.prefs);/*TODO: utiliser PreferenceFragment 
+	   addPreferencesFromResource(R.xml.prefs); /*TODO: utiliser PreferenceFragment 
 	   											plutot que preference activity pour eviter 
 	   											l'obsolescence*/
 	}
 }
+
+
