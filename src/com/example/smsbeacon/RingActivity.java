97b113fbadf1baf76b810c0ef70794f17df3ac2e
tnemelc <clement.daviller@gmail.com>
2013-07-27 13:34:01 +0200

    modif interface ring activity + gestion de de la durée de la sonnerie

--- src/com/example/smsbeacon/RingActivity.java
+++ src/com/example/smsbeacon/RingActivity.java
@@ -11,26 +11,35 @@ import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.preference.ListPreference;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.SeekBar;
 import android.widget.SeekBar.OnSeekBarChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class RingActivity extends Activity implements OnSeekBarChangeListener{
+public class RingActivity extends Activity implements OnSeekBarChangeListener {
 	private Camera m_cam = null;
 	private boolean m_hasFlash = false;
 	private Parameters m_param = null;
 	private Ringtone m_rgtone = null;
 	private int m_initVolume = 0;
+	private  TextView m_phoneNumber = null;
+	private String m_ringTime = null;
 	AudioManager m_audioManager;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ring);
-		SeekBar mBar;
-		mBar = (SeekBar) findViewById(R.id.seekbar);
-		mBar.setOnSeekBarChangeListener(this);
+		SeekBar m_Bar;
+		m_Bar = (SeekBar)findViewById(R.id.seekbar);
+		m_Bar.setOnSeekBarChangeListener(this);
+		m_phoneNumber = (TextView)findViewById(R.id.phone_number);
+		m_phoneNumber.setText("06.86.65.37.93 tried to locate you");
+		SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getBaseContext());
+		m_ringTime = prefs.getString(getBaseContext().getString(R.string.pref_home_key_time), "default choice");
 		m_hasFlash = getApplicationContext().getPackageManager()
 		        .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
 		m_audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
@@ -45,6 +54,34 @@ public class RingActivity extends Activity implements OnSeekBarChangeListener{
 			turnOnFlash();
 		}
 		turnOnRingTone();
+		new Thread(new Runnable(){
+			public void run(){
+			try {
+				if (m_ringTime.equals("time_elapse_30_sec"))
+				{
+					Thread.sleep(30000);
+				}
+				else if (m_ringTime.equals("time_elapse_1_min"))
+				{
+					Thread.sleep(60000);
+				}
+				else if (m_ringTime.equals("time_elapse_2_min"))
+				{
+					Thread.sleep(120000);
+				}
+				turnOffFlash();
+				turnOffRingTone();
+				m_audioManager.setStreamVolume(
+						   AudioManager.STREAM_RING,
+						   m_initVolume,
+						   AudioManager.FLAG_PLAY_SOUND
+						);
+				RingActivity.this.finish();
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			}
+			}).start();
 	}
 	
 	@Override
@@ -80,6 +117,7 @@ public class RingActivity extends Activity implements OnSeekBarChangeListener{
 	protected void turnOnRingTone()
 	{
 		Uri notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_RINGTONE);
+		
 		m_rgtone = RingtoneManager.getRingtone(getApplicationContext(), notification);
 		m_rgtone.play();
 	}
